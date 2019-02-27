44dc2fdaef77d82662df0ca53282ee7b58a8d6e3
Diogo Fernandes Freire <sirkrazzyakadiogoffreire@gmail.com>
2019-02-27 11:47:42 +0000

    Just my RC Class Project

---
+++ WebApplication2/WebApplication2/Controllers/adminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    public class adminController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            //ViewData["Greeting"] = "Qualquer coisa";
+            return View();
+        }
+
+        public IActionResult AddProduct(Product p)
+        {
+
+            return View();
+        }
+    }
+}
